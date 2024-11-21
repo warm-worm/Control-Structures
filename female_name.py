a03452436e705f729b121d8a2c77d8d87503b892
Aleksandra Korniak <ola.korniak@gmail.com>
2024-11-21 20:46:41 +0100

    Add files via upload

---
+++ female_name.py
@@ -0,0 +1,10 @@
+#
+## Program that prints the name entered from the keyboard, 
+## provided it is a female name
+#
+name = input('Enter the name: ')
+last_letter = name[-1]
+if last_letter == 'a':
+    print(f'{name}')
+else:
+    print('Enter a different name.')
